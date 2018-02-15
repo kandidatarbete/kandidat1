a146e9f13498e16d3a31f080963f361d4e268fbb
carlssss <35733409+carlssss@users.noreply.github.com>
2018-02-15 15:38:46 +0100

    fungerande iterering av linjärt system

--- yalmip_wilmer/Skapandeavmatrisyalmip.m
+++ yalmip_wilmer/Skapandeavmatrisyalmip.m
@@ -9,10 +9,16 @@ dz=sdpvar(Ns,Nv);
 ddz=sdpvar(Ns,Nv);
 %A=zeros(Nv,Nv)
 Asub=[1 h 0; 0 1 h; 0 0 1];
+Xtest=zeros(3*Nv,Ns);
+Xtest(2,1)=2;
+Xtest(5,1)=3;
+Xtest(8,1)=4;
+    
 for i=1:Nv
     X(1+(i-1)*3,:)=t(:,i);
     X(2+(i-1)*3,:)=z(:,i);
     X((3+(i-1)*3),:)=dz(:,i);
+    
 end
 %A(1:3,1:3)=Asub
 A=zeros(3*Nv,3*Nv);
@@ -22,7 +28,7 @@ for i=1:Nv
     %3*i
     A(3*i-2:3*i,3*i-2:3*i)=Asub;
 end
-X(:,k+1)=A*X(:,k);
+Xtest(:,k+1)=A*Xtest(:,k);
 %X(1,1)
-  
-  
+Xtest
+Xtest(2,2)

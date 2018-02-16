153a5c6571820593ef9d2f69a5001f0ae8b58ab6
Fredrik <fredrik@localhost.localdomain>
2018-02-16 10:19:02 +0100

    annoteringar

--- yalmip_wilmer/Skapandeavmatrisyalmip.m
+++ yalmip_wilmer/Skapandeavmatrisyalmip.m
@@ -1,19 +1,25 @@
 clear all, clc
-Ns=100;
-Nv=3;
-h=0.1;
+Ns=100; % number of states
+Nv=3; % number of vehicles
+h=0.1; % timestep
 k=1:Ns-1;
+
+% testing out sdpvar
 t=sdpvar(Ns,Nv);
 z=sdpvar(Ns,Nv);
 dz=sdpvar(Ns,Nv);
 ddz=sdpvar(Ns,Nv);
 %A=zeros(Nv,Nv)
-Asub=[1 h 0; 0 1 h; 0 0 1];
+
+Asub=[1 h 0; 0 1 h; 0 0 1]; % submarix used to build global transfermatrix
+
+% give a few vehicles initial speed
 Xtest=zeros(3*Nv,Ns);
 Xtest(2,1)=2;
 Xtest(5,1)=3;
 Xtest(8,1)=4;
-    
+
+% constructing state vector
 for i=1:Nv
     X(1+(i-1)*3,:)=t(:,i);
     X(2+(i-1)*3,:)=z(:,i);
@@ -22,15 +28,16 @@ for i=1:Nv
 end
 %A(1:3,1:3)=Asub
 A=zeros(3*Nv,3*Nv);
+
+% global A matrix is built with Asub as diagonal elements
 for i=1:Nv
-    %A(i+3*(i-1):3*i-2,i+3*(i-1):i+3*(i-1)+3)=Asub;
-    %3*i-2
-    %3*i
     A(3*i-2:3*i,3*i-2:3*i)=Asub;
 end
 for i=k
     Xtest(:,i+1)=A*Xtest(:,i);
 end
+
+% plot trajectories
 plot(Xtest(1,k),k);
 hold on
 plot(Xtest(4,k),k);

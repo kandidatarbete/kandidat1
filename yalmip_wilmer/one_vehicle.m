5c0838ccc76cbb52a62b93e7cddbb3388ac4e6c0
Fredrik <fredrik@localhost.localdomain>
2018-02-16 16:42:22 +0100

    fungerande kostnadsfunktion

--- yalmip_wilmer/one_vehicle.m
+++ yalmip_wilmer/one_vehicle.m
@@ -1,5 +1,4 @@
-% försök att implementera den otransformerade modellen utan kritiska
-% zon-constraints för ett fordon. 
+% försök att implementera den otransformerade modellen utan kritiskazon-constraints för ett fordon. 
 clear all, clc
 close all; 
 
@@ -9,27 +8,35 @@ t = sdpvar(1,Ns);
 p = sdpvar(1,Ns);
 v = sdpvar(1,Ns); 
 a = sdpvar(1,Ns); 
+sdpvar u; % styrvariabeln
 
 X = [p; v; a]; % planeringsrapporten sidan 1
 %A = [0 1 0; 0 0 1; 0 0 0];
-h = 0.1
+h = 0.1;
 A = [1 h 0; 0 1 h; 0 0 0]; 
 
 % definera X(t=0)
-X(2) = 5; % initial speed
+X(2) = 0.1; % initial speed
 X(1) = 0; % initial position
 X(3) = 0; % initial acceleration
 
 % huvudregel
-% X_{k+1} = A*X_k \forall k, en constraint per sample och variabel
+% X_{k+1} = A*X_k \forall k, en constraint per sample
 constraints  = [];
 for k = 1:(Ns-1)
-    constraints = [constraints, X(:,k+1) == A*X(:,k)];  % + h * u 
+    constraints = [constraints, X(:,k+1) == A*X(:,k) + u*h*[0; 0; 1]];  % Ax + h * u 
    
     
 end
 
-cost = []; 
+% ett fordon kan inte gasa/bromsa hur hårt som helst
+constraints = [constraints, (-5 <= a <= 5)]; 
+
+constraints = [constraints, (-5 <= u <= 5)]; 
+
+cost = [1/sum(X(1,:))]; % se till att dom kommer så långt som möjligt
+%cost = [sum(X(1,:))]; % se till att dom kommer så kort som möjligt
+%cost = [];
 
 options     = sdpsettings('verbose',0,'debug', 1); 
 sol         = optimize(constraints, sum(cost), options); 
@@ -46,3 +53,10 @@ else
 end
 
 value(X)
+
+% plot 
+plot(1:Ns,value(X(1,:)));
+xlabel('sample no');
+ylabel('position'); 
+
+

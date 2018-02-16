3d101cf94ee0ad14f8a7887adcf84e0be68b0338
carlssss <35733409+carlssss@users.noreply.github.com>
2018-02-16 10:26:24 +0100

    lagt till explicit uttryck för b och u's startvärden

--- yalmip_wilmer/Skapandeavmatrisyalmip.m
+++ yalmip_wilmer/Skapandeavmatrisyalmip.m
@@ -3,17 +3,20 @@ Ns=100;
 Nv=3;
 h=0.1;
 k=1:Ns-1;
+%sdpvar verkar vara ett yalmipverktyg, kanske att matriserna är variabler i
+%hpipm
 t=sdpvar(Ns,Nv);
 z=sdpvar(Ns,Nv);
 dz=sdpvar(Ns,Nv);
 ddz=sdpvar(Ns,Nv);
 %A=zeros(Nv,Nv)
 Asub=[1 h 0; 0 1 h; 0 0 1];
+%Xtest är en matris med värden för att testa A-matrisen
 Xtest=zeros(3*Nv,Ns);
 Xtest(2,1)=2;
 Xtest(5,1)=3;
 Xtest(8,1)=4;
-    
+
 for i=1:Nv
     X(1+(i-1)*3,:)=t(:,i);
     X(2+(i-1)*3,:)=z(:,i);
@@ -27,6 +30,14 @@ for i=1:Nv
     %3*i-2
     %3*i
     A(3*i-2:3*i,3*i-2:3*i)=Asub;
+end
+B=zeros(3*Nv,1)
+u=zeros(3,Ns);
+u(1,1)=Xtest(2,1);
+u(2,1)=Xtest(5,1);
+u(3,1)=Xtest(8,1);
+for i =1:Nv
+    B(3*i,1)=h;
 end
 for i=k
     Xtest(:,i+1)=A*Xtest(:,i);

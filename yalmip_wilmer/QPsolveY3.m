e2178fa3e2837b0cf5313a1c9f1d127833c6d04b
carlssss <35733409+carlssss@users.noreply.github.com>
2018-02-20 11:52:29 +0100

    constraints för tid fixade ish

--- yalmip_wilmer/QPsolveY3.m
+++ yalmip_wilmer/QPsolveY3.m
@@ -6,7 +6,6 @@ task.Ns=numel(task.s);
 task.Nv=3;                          % number of vehicles (cheese within 2-4)
 task.I=intersection;
 
-
 % loop on al possible permutations of crossing orders. Note that Yalmip is not
 % meant to be called iteratively, so you may want to replace it with
 % another algorithm that is better suited for this purpose. 
@@ -18,6 +17,8 @@ se=ss+task.I.criticalzone;          %[m] distance at which the vehicle exits the
 entryangle=[0; 0.5; 1; 1.5;0;0]*pi; %[rad] angle at which the vehicles enter the critical zone
 exitangle=[0; 1; 1.5; 1.5; 0;0]*pi; %[rad] angle at which the vehicles exit the critical zone
 vref=[47; 48; 50; 49; 50; 50]/3.6;  %[m/s] reference speed for the vehicles (the first task.Nv elements are used)
+
+
 
 
 
@@ -54,14 +55,24 @@ task.Wv=1; task.Wdv=1; task.Wddv=0.5;
 
 
 %%
-clear all, clc
+clc,clf
 close all; 
-
+St=task.St;
+Sz=task.Sz;
+Sdz=task.Sdz;
+Sddz=task.Sddz;
 Ns = 100; % number of samples
 Nv = 2; 
 k=1:Ns-1;
 % yalmip-specifikt, referera hädanefter uteslutande till X(i), index i 
 % specificerar fordon och variabel
+for i=1:Nv
+t(i,:) = sdpvar(1,Ns); 
+z(i,:) = sdpvar(1,Ns);
+dz(i,:) = sdpvar(1,Ns); 
+ddz(i,:) = sdpvar(1,Ns); 
+    
+end
 for i = 1:Nv 
 t(i,:) = sdpvar(1,Ns); 
 p(i,:) = sdpvar(1,Ns);
@@ -76,15 +87,15 @@ end
  % styrvariabeln
 for i=1:Nv
     
-    X(3*i-2,:)=p(i,:);
-    X(3*i-1,:)=v(i,:);
-    X(3*i,:)=a(i,:);
+    X(3*i-2,:)=t(i,:);
+    X(3*i-1,:)=z(i,:);
+    X(3*i,:)=dz(i,:);
     %X = [p(i,:); v(i,:); a(i,:)]; % planeringsrapporten sidan 1
 end
 %X = [p(1,:); v(1,:); a(1,:)]; % planeringsrapporten sidan 1
 %A = [0 1 0; 0 0 1; 0 0 0];
 h = 0.1;
-Asub = [1 h 0; 0 1 h; 0 0 0]; 
+Asub = [1 h 0; 0 1 h; 0 0 1]; 
 
 % konstruera generaliserade A: 
 for i=1:Nv
@@ -95,22 +106,36 @@ end
 X(2) = 0.1; % initial speed
 X(1) = 0; % initial position
 X(3) = 0; % initial acceleration
-
+for i=1:Nv
+    
+    X(3*i-2,1)=0;%inital time
+    X(3*i-1)=rand*10;
+    X(3*i)=0;
+end
 % huvudregel
 % X_{k+1} = A*X_k \forall k, en constraint per sample
 constraints  = [];
-for k=1:Ns-1
-    constraints = [constraints, X(:,k+1) == A*X(:,k) + U(:,k)*h];  % Ax + h * u 
-   
+
+constraints = [constraints, X(:,k+1) == A*X(:,k) + U(:,k)*h];  % Ax + h * u 
     
-end
+
 
 % ett fordon kan inte gasa/bromsa hur hårt som helst
 constraints = [constraints, (-5 <= a <= 5)]; 
 
-constraints = [constraints, (-5 <= U(:,k) <= 5)]; 
+constraints = [constraints, (-5 <= U(:,k) <= 5)];
+%detta constraintet verkar ta tid
+%ger ekvationen f�r hur tiden varierar med stegen i position
+for i=1:Nv
+    constraints=[constraints, 
+    X(3*i-2,2:Ns)==X(3*i-2,1:Ns-1)+ h*X(3*i-1,1:Ns-1)*Sz/St]
+end
+% 
+for i=1:Nv
+     constraints=[constraints, X(3*i-1)>0];
+ end
 
-cost = [sum(X(1,:))]; % se till att dom kommer så långt som möjligt
+cost = [1/sum(X(2,:))+1/sum(X(5,:))]; % se till att dom kommer så långt som möjligt
 
 options     = sdpsettings('verbose',0,'debug', 1); 
 sol         = optimize(constraints, sum(cost), options); 
@@ -127,19 +152,20 @@ else
  yalmiperror(sol.problem)
 end
 
-value(X)
+value(X);
 
 % plot 
 
 
-for i=1:Nv
-    plot(1:Ns,value(X(3*i-2,:)));
+for i=1:1
+    %plot(value(X(3*i-2,:)),value(X(3*i-1)));
+    
     hold on
 end
 %plot(1:Ns,value(X(1,:)));
 hold on
 %plot(1:Ns,value(X(4,:)));
 xlabel('sample no');
-ylabel('position'); 
+ylabel('lethargy'); 
 
-
+value(X(1,:))

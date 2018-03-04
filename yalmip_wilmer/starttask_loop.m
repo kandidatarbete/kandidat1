743b7944ec2710c58874a121fafb9d854ffae40f
carlssss <35733409+carlssss@users.noreply.github.com>
2018-03-04 14:30:15 +0100

    försök till att ha fungerande criticalzone

--- yalmip_wilmer/starttask_loop.m
+++ yalmip_wilmer/starttask_loop.m
@@ -15,7 +15,13 @@ looporder=1;
 for (i =1:looporder)
     animate_res_new(task);
     % rebuild task from previous task and reoptimize
-    task = remove_vehicle(1,task); 
+    past=past_criticalzone(task,current_index);%returnar en array som säger vilka som är utanför
+    for(i=1:Nv)
+        if(past(i))
+            task=remove_vehicle(i,task);
+        end
+    end
+    %task = remove_vehicle(1,task); 
     task = optimize_task(task);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

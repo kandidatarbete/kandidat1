517d04a98d16b615638365f655bb0402c32e4607
carlssss <35733409+carlssss@users.noreply.github.com>
2018-03-21 14:36:44 +0100

    Fältet ax som är accelerationen har lagts till i res

--- yalmip_wilmer/optimize_task.m
+++ yalmip_wilmer/optimize_task.m
@@ -8,13 +8,14 @@ resopt.cost=Inf;
 for j=1:size(task.crossorderperm,1)
     task.crossingorder=task.crossorderperm(j,:);
     %res=QPsolveY4(task);
-    res=QPsolveY5(task);
-    %res = QPsolveY6(task);
+    %res=QPsolveY5(task);
+    res = QPsolveY6(task);
+    %res=QPsolveY8(task);
     ttot=ttot+res.time(end);
-    ax=diff(res.v)./diff(res.t); ax=[ax;ax(end,:)];
+    res.ax=diff(res.v)./diff(res.t); res.ax=[res.ax;res.ax(end,:)];
     fprintf('%s: order=%s, cost=%1.4f, vx~[%1.0f,%1.0f]km/h, ax~[%1.1f,%1.1f]m/s2, t=%1.2f ms\n', ...
         res.status, sprintf('%d',task.crossingorder), res.cost, min(res.v(:))*3.6, max(res.v(:))*3.6, ...
-        min(ax(:)), max(ax(:)), res.time(end)*1000);
+        min(res.ax(:)), max(res.ax(:)), res.time(end)*1000);
     
     if res.cost < resopt.cost
         resopt=res;

ba6128b1d0427aa25c8207b081c1bd81ad273fed
Fredrik <fredrik@localhost.localdomain>
2018-04-23 13:53:49 +0200

    cleanup

--- yalmip_wilmer/QPsolveY5_new.m
+++ yalmip_wilmer/QPsolveY5_new.m
@@ -46,8 +46,6 @@ for i = 1:Nv
 end
 constraints  = [];
 
-
-%Xhat2 = zeros(4*Nv*Ns,1); 
 Xhat2=[];
 for i = 0:Nv-1
     Xhat2 = [Xhat2; Xhat(:,i+1)]; 
@@ -96,103 +94,61 @@ disp(size(Acol));
 
 % box constraints
 constraints=[constraints; Acol*Xhat2 == 0]; 
-%matris för vänsterled i ekvation för startvärden
+%matris fï¿½r vï¿½nsterled i ekvation fï¿½r startvï¿½rden
 Aeq2=zeros(4*Ns);
 
 for i=1:3
     Aeq2(i,i)=1;
 end
 
+
+% 
+% %trying to create a matrix A which satisfies AX=B for the initial valuees
+% %04-22
+% Asub=eye(3);
+% A=zeros(3*Nv,4*Ns*Nv);
+% j=1;
+% i=1;
+% while(j<=3*Nv)
+%     A(j:j+2,i:i+2)=Asub;
+%     i=i+4*Ns;
+%     j=j+3;
+% end
+% B=zeros(3*Nv,1);
+% 
+% for i=1:Nv
+%     B(i)=0;
+%     B(i+1)=1/vstart(i)/Sz;
+%     B(i+2)=0;
+%     
+% end
 beq2 = zeros(4*Ns,Nv);
 Aeq11 = zeros(1,4*Ns); 
 Aeq12 = zeros(1,4*Ns);
-
-%trying to create a matrix A which satisfiest AX=B for the initial valuees
-%04-22
-Asub=eye(3);
-A=zeros(3*Nv,4*Ns*Nv);
-j=1;
-i=1;
-while(j<=3*Nv)
-    A(j:j+2,i:i+2)=Asub;
-    i=i+4*Ns;
-    j=j+3;
-end
-B=zeros(3*Nv,1);
-
 for i=1:Nv
-    B(i)=0;
-    B(i+1)=1/vstart(i)/Sz;
-    B(i+2)=0;
+    c1 = -amin*3*vref*Sz/(vref^3)/Sdz;
+    c2 = 2*amin/vref^3/Sdz;
+    c3 = -V(i).axmax*3*vref*Sz/(vref^3)/Sdz;
+    c4 = V(i).axmax*2/vref^3/Sdz;
+    %rhs for initial values
+    beq2(1, i) = 0;
+    beq2(2,i) = 1/vstart(i)/Sz;
+    beq2(3,i) = 0;
     
-end
-disp(size(B))
-disp(size(A))
-%disp(A)
-%constraints=[constraints, A*Xhat2==B];
-
-
-
-for i=1:Nv
-        c1 = -amin*3*vref*Sz/(vref^3)/Sdz;
-        c2 = 2*amin/vref^3/Sdz;
-        c3 = -V(i).axmax*3*vref*Sz/(vref^3)/Sdz;
-        c4 = V(i).axmax*2/vref^3/Sdz;
-        %högerledet i ekvationen för startvärden
-        beq2(1, i) = 0;
-        beq2(2,i) = 1/vstart(i)/Sz;
-        beq2(3,i) = 0;
-        
     for j = 1:Ns
-        % equality constraints
-
+        Aeq11(4*j-1) = 1;
+        Aeq11(4*j-2) = -c1;
+        b1(1,i) = c2;
         
-        % less than constraints
-
-        %constraints = [constraints, X(3*i,j) - c1*X(3*i-1,j)  <= c2];
-        %equation 1
-        %constraints=[constraints, X(3*i-1,:)<=1/V(i).vxmin/Sz];
-        
-        % greater than constraints
-
-        %constraints = [constraints, -X(3*i,j) + c3*X(3*i-1,j) <= c4];
-        % equation 2
-        
-
-       Aeq11(4*j-1) = 1;
-       Aeq11(4*j-2) = -c1;
-       b1(1,i) = c2;
-       
-       Aeq12(4*j -1) = -1; 
-       Aeq12(4*j -2) = c3;
-       b2(1,i) = c4; % todo build this in terms of Xhat2 and append to Acol
-       
-       %cond = zeros(2,4*Ns*Nv); 
-       %cond(1,ddzind(j,i)) = 1; 
-       %cond(1,zind(j,i)) = -c1; 
-       
-       %Acol = [Acol; cond];
-       % do not forget appending to bcol 
-       % also move counstraints appending to below this loop
-        
+        Aeq12(4*j -1) = -1;
+        Aeq12(4*j -2) = c3;
+        b2(1,i) = c4; % todo build this in terms of Xhat2 and append to Acol    
     end
 end
-% box constraints
-% equation 1
 Aineq_f = [Aeq11;Aeq12];
 bineq_f = [b1;b2];
 constraints = [constraints, Aineq_f*Xhat <= bineq_f]; 
 constraints=[constraints, Aeq2*Xhat==beq2];
-
-% critical zone constraint, todo: append constraints as a general box
-% constraint
-% sample i vehicle j 
-% tind = @(i,j) 4*i-3 + 4*Ns*(j-1);
-% zind = @(i,j) 4*i-2 + 4*Ns*(j-1);
-% dzind = @(i,j) 4*i-1 + 4*Ns*(j-1); 
-% uind = @(i,j) 4*i + 4*Ns*(j-1);
-% disp(size(Xhat2));
-
 
 for i = 1:Nv-1
     Aoc = zeros(Nv,4*Ns);
@@ -200,24 +156,21 @@ for i = 1:Nv-1
     ind2 = co(i);
     ind3 = 4*V(co(i+1)).Nzs-3;
     ind4 = co(i+1);
-    constraints = [constraints, Xhat(ind1,ind2) - Xhat(ind3,ind4) <= 0]; 
+    constraints = [constraints, Xhat(ind1,ind2) - Xhat(ind3,ind4) <= 0];
     
-         %t(V(co(j)).Nze, co(j)) <= t(V(co(j+1)).Nzs,co(j+1)) ];
-         sample1 = V(co(i)).Nze; 
-         vehicle1 = co(i); 
-         xind1 = tind(sample1,vehicle1); 
-         
-         sample2 = V(co(i+1)).Nzs; 
-         vehicle2 = co(i+1); 
-         xind2=tind(sample2,vehicle2); 
-         constraints = [constraints, Xhat2(xind1) <= Xhat2(xind2)]; % append this condition to Acol, not Aineq
-         
-         cond = zeros(1,4*Nv*Ns); 
-         cond(1,xind1) = 1; 
-         cond(1,xind2) = -1; 
-         %disp(size(Acol)); 
-         %disp(size(cond)); 
-         Acol = [Acol;cond]; 
+    sample1 = V(co(i)).Nze;
+    vehicle1 = co(i);
+    xind1 = tind(sample1,vehicle1);
+    
+    sample2 = V(co(i+1)).Nzs;
+    vehicle2 = co(i+1);
+    xind2=tind(sample2,vehicle2);
+    constraints = [constraints, Xhat2(xind1) <= Xhat2(xind2)]; % append this condition to Acol, not Aineq
+    
+    cond = zeros(1,4*Nv*Ns);
+    cond(1,xind1) = 1;
+    cond(1,xind2) = -1;
+    Acol = [Acol;cond];
     
 end
 
